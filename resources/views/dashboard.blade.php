c96a0da3645305907cf17166b36481f021d38b33
toussaintstromy <61969811+rutagarama@users.noreply.github.com>
2022-01-09 19:18:36 +0200

    adding roles

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,19 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Dashboard') }}
+        </h2>
+    </x-slot>
+
+    <div class="py-12">
+        <div class="ml-4 sm:px-6 lg:px-8">
+            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+
+                <div class="p-6 bg-white">
+                   Great!! You're in!
+                </div>
+
+            </div>
+        </div>
+    </div>
+</x-app-layout>
